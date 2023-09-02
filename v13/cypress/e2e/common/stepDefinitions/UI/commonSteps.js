522f679a944897a6dabcfc4038ae225edd91d19d
far11ven <far11ven@gmail.com>
2023-09-02 14:08:27 +0100

    updated nightly.yml; code formattting

--- v13/cypress/e2e/common/stepDefinitions/UI/commonSteps.js
+++ v13/cypress/e2e/common/stepDefinitions/UI/commonSteps.js
@@ -4,34 +4,25 @@ import HomePage from '../../pages/HomePage/HomePage';
 import ResultPage from '../../pages/ResultPage/ResultPage';
 const addContext = require('mochawesome/addContext');
 
-const homePage = new HomePage();
-const resultPage = new ResultPage();
+const homePage = new HomePage()
+const resultPage = new ResultPage()
 
 Given('I open the Google web url', () => {
-    cy.visit('/');
-    cy.reportLog("This is Subject");
-    cy.reportLogKV("This is key!","This is value!");
-  });
-  
-Then(
-    'I verify title of web page as {string}',
-    (title) => {
-        cy.title().should('include', title);
-    }
-  );
+	cy.visit('/')
+	cy.reportLog("This is Subject")
+	cy.reportLogKV("This is key!", "This is value!")
+})
 
-When(
-    'I provide search query as {string}',
-    (query) => {
-      homePage.clickSearchTxtBox();
-      homePage.typeInSearchTxtBox(query);
-      homePage.submitSearchQuery();
-    }
-  );
+Then('I verify title of web page as {string}', (title) => {
+	cy.title().should('include', title)
+})
 
-Then(
-    'Verify first search result to match {string} keyword',
-    (search_keyword) => {
-      let result = resultPage.verifyFirstResult(search_keyword);
-    }
-  );
+When('I provide search query as {string}', (query) => {
+	homePage.clickSearchTxtBox()
+	homePage.typeInSearchTxtBox(query)
+	homePage.submitSearchQuery()
+})
+
+Then('Verify first search result to match {string} keyword', (search_keyword) => {
+	let result = resultPage.verifyFirstResult(search_keyword)
+})
